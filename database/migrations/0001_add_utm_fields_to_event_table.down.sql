fe94aa39f84e5f24fdc98bdc45d1cd2828499489
Rahul Kadyan <hi@znck.me>
2020-05-12 21:37:43 +0530

    feat(database): add utm fields to events

---
+++ database/migrations/0001_add_utm_fields_to_event_table.down.sql
@@ -0,0 +1,4 @@
+ALTER TABLE
+  "event" DROP COLUMN "referrer",
+  DROP COLUMN "medium",
+  DROP COLUMN "campaign";
---
+++ database/migrations/0001_add_utm_fields_to_event_table.up.sql
@@ -0,0 +1,8 @@
+ALTER TABLE
+  "event"
+ADD
+  COLUMN "referrer" character varying,
+ADD
+  COLUMN "medium" character varying,
+ADD
+  COLUMN "campaign" character varying;
