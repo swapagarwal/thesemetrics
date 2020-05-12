2c289ff6051af0a27f1e0d843ca25dffea364827
Rahul Kadyan <hi@znck.me>
2020-05-12 17:03:53 +0530

    fix(database): quote table names, use single quotes in jsonb default value and correct typo

--- database/migrations/0000_create_database.up.sql
+++ database/migrations/0000_create_database.up.sql
@@ -17,14 +17,14 @@ CREATE TABLE "user" (
 );
 
 CREATE TABLE "team_member" (
-  "teamId" integer NOT NULL REFERENCES team("id") ON DELETE CASCADE,
-  "userId" integer NOT NULL REFERENCES user("id") ON DELETE CASCADE,
+  "teamId" integer NOT NULL REFERENCES "team"("id") ON DELETE CASCADE,
+  "userId" integer NOT NULL REFERENCES "user"("id") ON DELETE CASCADE,
   "role" character varying NOT NULL,
   PRIMARY KEY ("teamId", "userId")
 );
 
 CREATE TABLE "project" (
-  "teamId" integer NOT NULL REFERENCES team("id") ON DELETE CASCADE,
+  "teamId" integer NOT NULL REFERENCES "team"("id") ON DELETE CASCADE,
   "id" integer NOT NULL,
   "name" character varying NOT NULL,
   "type" character varying NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE "project" (
 );
 
 CREATE TABLE "event" (
-  "projectId" integer NOT NULL REFERENCES project("id") ON DELETE CASCADE,
+  "projectId" integer NOT NULL REFERENCES "team"("id") ON DELETE CASCADE,
   -- event/resource
   "id" serial NOT NULL,
   "type" character varying NOT NULL,
@@ -58,7 +58,7 @@ CREATE TABLE "event" (
   "userTimestamp" timestamp without time zone,
   -- extra
   "batchId" character varying,
-  "data" jsonb NOT NULL DEFAULT "{}"::jsonb,
-  "createdAt" timestamp with time zone NOT NULL DEFAUTL current_timestamp,
+  "data" jsonb NOT NULL DEFAULT '{}'::jsonb,
+  "createdAt" timestamp with time zone NOT NULL DEFAULT current_timestamp,
   PRIMARY KEY ("id")
 );
