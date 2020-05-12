65532dd1ad34464eba43af37d33e1640dfccec9a
Rahul Kadyan <hi@znck.me>
2020-05-13 04:31:30 +0530

    chore: add __DEV__ branch support

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,7 @@
 import Path from 'path'
 import typescript from 'rollup-plugin-typescript2'
-// import dts from 'rollup-plugin-dts'
-
-export default configs
+import resolve from '@rollup/plugin-node-resolve'
+import replace from '@rollup/plugin-replace'
 
 function getPackageDirectory(packageName) {
   return Path.join(__dirname, packageName)
@@ -20,32 +19,69 @@ function input(packageName) {
   return getPackageFile(packageName, 'src/index.ts')
 }
 
-function output(packageName) {
+function output(packageName, kind = undefined) {
   const f = (fieldName) => {
     const fileName = getPackageField(packageName, fieldName)
 
     return fileName ? getPackageFile(packageName, fileName) : null
   }
 
-  return [
-    { file: f('main'), format: 'cjs' },
-    { file: f('module'), format: 'esm' },
+  const build = getPackageField(packageName, 'build') || {}
+
+  const outputs = [
+    !kind || kind === 'main' ? { file: f('main'), format: 'cjs' } : {},
+    !kind || kind === 'module' ? { file: f('module'), format: 'esm' } : {},
+    !kind || kind === 'browser'
+      ? { file: f('browser'), format: 'umd', name: build.name }
+      : {},
   ].filter((output) => !!output.file)
+
+  return outputs
 }
 
-const packages = ['database']
+function getExternal(packageName) {
+  return [
+    ...Object.keys(getPackageField(packageName, 'dependencies') || {}),
+    ...Object.keys(getPackageField(packageName, 'peerDependencies') || {}),
+  ]
+}
+
+const packages = ['database', 'pixel', 'clients/javascript']
 
 /** @type {import('rollup').RollupOptions[]} */
-const configs = [
-  ...packages.map((packageName) => ({
-    input: input(packageName),
-    output: output(packageName),
+const configs = []
+
+export default configs
+
+packages.forEach((packageName) => {
+  const build = getPackageField(packageName, 'build') || {}
+  const config = {
     plugins: [
+      replace({
+        __DEV__: process.env.BUILD !== 'production',
+      }),
+      resolve(),
       typescript({
         check: true,
         tsconfig: getPackageFile(packageName, 'tsconfig.json'),
       }),
     ],
-    external: Object.keys(getPackageField(packageName, 'peerDependencies')),
-  })),
-]
+    external: getExternal(packageName),
+  }
+
+  if (build.input) {
+    Object.entries(build.input).forEach(([kind, entry]) => {
+      configs.push({
+        ...config,
+        input: getPackageFile(packageName, entry),
+        output: output(packageName, kind),
+      })
+    })
+  } else {
+    configs.push({
+      ...config,
+      input: input(packageName),
+      output: output(packageName),
+    })
+  }
+})
