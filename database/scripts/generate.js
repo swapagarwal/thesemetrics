f4d4ba6eff832d1814d860f6c48f6e9a10f00194
Rahul Kadyan <hi@znck.me>
2020-05-12 16:41:24 +0530

    chore(database): add typeorm and sql-to-js migration generation script

---
+++ database/scripts/generate.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+const Path = require('path')
+const FileSystem = require('fs').promises
+
+const migrationsDir = Path.resolve(__dirname, '..', 'migrations')
+
+function indent(content, width = 6) {
+  const prefix = ' '.repeat(width)
+
+  return content
+    .trim()
+    .split('\n')
+    .map((line) => prefix + line)
+    .join('\n')
+}
+
+async function main() {
+  const files = (await FileSystem.readdir(migrationsDir)).filter((fileName) =>
+    /^\d{4}_.*\.sql$/.test(fileName)
+  )
+
+  const migrations = Array.from(
+    new Set(files.map((fileName) => fileName.replace(/\.(up|down)\.sql$/, '')))
+  )
+
+  async function getQueries(fileName) {
+    if (!files.includes(fileName)) return []
+    const contents = await FileSystem.readFile(
+      Path.resolve(migrationsDir, fileName),
+      { encoding: 'utf8' }
+    )
+
+    return contents.split(';')
+  }
+
+  async function getUpQueries(migration) {
+    return getQueries(migration + '.up.sql')
+  }
+
+  async function getDownQueries(migration) {
+    return getQueries(migration + '.down.sql')
+  }
+
+  migrations.map(async (migration) => {
+    const migrationFile = Path.resolve(migrationsDir, migration + '.js')
+    const [id, name] = migration.split('_', 2)
+    const className = `${name.replace(/[^a-zA-Z0-9$_]+/g, '_')}_158928020${id}`
+
+    await FileSystem.writeFile(
+      migrationFile,
+      [
+        `module.exports = class ${className} {`,
+        `  async up(runner) {`,
+        ...(await getUpQueries(migration)).map(
+          (query) => '    await runner.query(`\n' + indent(query) + '\n    `)'
+        ),
+        `  }`,
+        ``,
+        `  down(runner) {`,
+        ...(await getDownQueries(migration)).map(
+          (query) => '    await runner.query(`\n' + indent(query) + '\n    `)'
+        ),
+        `  }`,
+        `}`,
+      ].join('\n')
+    )
+  })
+}
+
+main().catch(console.error)
