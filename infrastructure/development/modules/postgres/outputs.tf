707324c0ae0ff76b774b46f9ebab2b2bc8a86cb1
Rahul Kadyan <hi@znck.me>
2020-05-13 18:26:58 +0530

    fix(infra): use ip_address as network_data is not available, anyways it is only needed for development

--- infrastructure/development/modules/postgres/outputs.tf
+++ infrastructure/development/modules/postgres/outputs.tf
@@ -1,4 +1,4 @@
 output "url" {
-  value = "postgresql://user:pass@${docker_container.postgres.network_data.ip_address}:5432/analytisc"
+  value = "postgresql://user:pass@${docker_container.postgres.ip_address}:5432/analytisc"
   description = "Database URI"
 }
