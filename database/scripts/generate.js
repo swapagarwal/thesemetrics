9e1a2e4bfc41cac2c66b371248c7d685c9e742e6
Rahul Kadyan <hi@znck.me>
2020-05-15 05:36:19 +0530

    fix(databate): use magic comment (-->) to separate queries

--- database/scripts/generate.js
+++ database/scripts/generate.js
@@ -1,74 +1,84 @@
 #!/usr/bin/env node
 
-const Path = require('path')
-const FileSystem = require('fs').promises
+const Path = require('path');
+const FileSystem = require('fs').promises;
 
-const migrationsDir = Path.resolve(__dirname, '..', 'migrations')
+const migrationsDir = Path.resolve(__dirname, '..', 'migrations');
 
+/**
+ * @param {string} content
+ * @param {number} width
+ */
 function indent(content, width = 6) {
-  const prefix = ' '.repeat(width)
+  const prefix = ' '.repeat(width);
 
   return content
     .trim()
     .split('\n')
     .map((line) => prefix + line)
-    .join('\n')
+    .join('\n');
 }
 
 async function main() {
   const files = (await FileSystem.readdir(migrationsDir)).filter((fileName) =>
-    /^\d{4}_.*\.sql$/.test(fileName)
-  )
+    /^\d{4}_.*\.(up|down)\.sql$/.test(fileName)
+  );
 
-  const migrations = Array.from(
-    new Set(files.map((fileName) => fileName.replace(/\.(up|down)\.sql$/, '')))
-  )
+  const migrations = Array.from(new Set(files.map((fileName) => fileName.replace(/\.(up|down)\.sql$/, ''))));
 
+  /**
+   * @param {string} fileName
+   */
   async function getQueries(fileName) {
-    if (!files.includes(fileName)) return []
-    const contents = await FileSystem.readFile(
-      Path.resolve(migrationsDir, fileName),
-      { encoding: 'utf8' }
-    )
+    if (!files.includes(fileName)) return [];
+    const contents = await FileSystem.readFile(Path.resolve(migrationsDir, fileName), { encoding: 'utf8' });
 
-    return contents.split(';').filter((str) => !!str.trim())
+    return contents
+      .split('-->')
+      .map((str) => str.trim().replace(/;$/, ''))
+      .filter((str) => !!str.trim());
   }
 
+  /**
+   * @param {string} migration
+   */
   async function getUpQueries(migration) {
-    return getQueries(migration + '.up.sql')
+    return getQueries(migration + '.up.sql');
   }
 
+  /**
+   * @param {string} migration
+   */
   async function getDownQueries(migration) {
-    return getQueries(migration + '.down.sql')
+    return getQueries(migration + '.down.sql');
   }
 
-  migrations.map(async (migration) => {
-    const migrationFile = Path.resolve(migrationsDir, migration + '.js')
-    const [, id, name] = /^(\d{4})_(.*)$/.exec(migration)
-    const className = `Migration_${name.replace(
-      /[^a-zA-Z0-9$_]+/g,
-      '_'
-    )}_158928020${id}`
+  await Promise.all(
+    migrations.map(async (migration) => {
+      const migrationFile = Path.resolve(migrationsDir, migration + '.js');
+      const result = /^(\d{4})_(.*)$/.exec(migration);
+      if (!result) return;
+      const [, id, name] = result;
+      const className = `Migration_${name.replace(/[^a-zA-Z0-9$_]+/g, '_')}_158928020${id}`;
 
-    await FileSystem.writeFile(
-      migrationFile,
-      [
-        `module.exports = class ${className} {`,
-        `  async up(runner) {`,
-        ...(await getUpQueries(migration)).map(
-          (query) => '    await runner.query(`\n' + indent(query) + '\n    `)'
-        ),
-        `  }`,
-        ``,
-        `  async down(runner) {`,
-        ...(await getDownQueries(migration)).map(
-          (query) => '    await runner.query(`\n' + indent(query) + '\n    `)'
-        ),
-        `  }`,
-        `}`,
-      ].join('\n')
-    )
-  })
+      await FileSystem.writeFile(
+        migrationFile,
+        [
+          `module.exports = class ${className} {`,
+          `  async up(runner) {`,
+          ...(await getUpQueries(migration)).map((query) => '    await runner.query(`\n' + indent(query) + '\n    `)'),
+          `  }`,
+          ``,
+          `  async down(runner) {`,
+          ...(await getDownQueries(migration)).map(
+            (query) => '    await runner.query(`\n' + indent(query) + '\n    `)'
+          ),
+          `  }`,
+          `}`,
+        ].join('\n')
+      );
+    })
+  );
 }
 
-main().catch(console.error)
+main().catch(console.error);
