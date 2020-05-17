f3496fd40799bf20ffa277727df1e8d14c18531f
Rahul Kadyan <hi@znck.me>
2020-05-17 23:22:41 +0530

    chore: use build.external to externalize packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,60 +1,61 @@
-import Path from 'path'
-import typescript from 'rollup-plugin-typescript2'
-import resolve from '@rollup/plugin-node-resolve'
-import replace from '@rollup/plugin-replace'
+import Path from 'path';
+import typescript from 'rollup-plugin-typescript2';
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
 
 function getPackageDirectory(packageName) {
-  return Path.join(__dirname, packageName)
+  return Path.join(__dirname, packageName);
 }
 
 function getPackageFile(packageName, fileName) {
-  return Path.join(__dirname, packageName, fileName)
+  return Path.join(__dirname, packageName, fileName);
 }
 
 function getPackageField(packageName, fieldName) {
-  return require(Path.join(__dirname, packageName, 'package.json'))[fieldName]
+  return require(Path.join(__dirname, packageName, 'package.json'))[fieldName];
 }
 
 function input(packageName) {
-  return getPackageFile(packageName, 'src/index.ts')
+  return getPackageFile(packageName, 'src/index.ts');
 }
 
 function output(packageName, kind = undefined) {
   const f = (fieldName) => {
-    const fileName = getPackageField(packageName, fieldName)
+    const fileName = getPackageField(packageName, fieldName);
 
-    return fileName ? getPackageFile(packageName, fileName) : null
-  }
+    return fileName ? getPackageFile(packageName, fileName) : null;
+  };
 
-  const build = getPackageField(packageName, 'build') || {}
+  const build = getPackageField(packageName, 'build') || {};
 
   const outputs = [
     !kind || kind === 'main' ? { file: f('main'), format: 'cjs', sourcemap: true } : {},
     !kind || kind === 'module' ? { file: f('module'), format: 'esm', sourcemap: true } : {},
-    !kind || kind === 'browser'
-      ? { file: f('browser'), format: 'umd', name: build.name, sourcemap: true }
-      : {},
-  ].filter((output) => !!output.file)
+    !kind || kind === 'browser' ? { file: f('browser'), format: 'umd', name: build.name, sourcemap: true } : {},
+  ].filter((output) => !!output.file);
 
-  return outputs
+  return outputs;
 }
 
 function getExternal(packageName) {
+  const build = getPackageField(packageName, 'build') || {};
+
   return [
     ...Object.keys(getPackageField(packageName, 'dependencies') || {}),
     ...Object.keys(getPackageField(packageName, 'peerDependencies') || {}),
-  ]
+    ...(build.external || []),
+  ];
 }
 
-const packages = ['database', 'app', 'pixel', 'clients/javascript']
+const packages = ['database', 'app', 'pixel', 'clients/javascript'];
 
 /** @type {import('rollup').RollupOptions[]} */
-const configs = []
+const configs = [];
 
-export default configs
+export default configs;
 
 packages.forEach((packageName) => {
-  const build = getPackageField(packageName, 'build') || {}
+  const build = getPackageField(packageName, 'build') || {};
   const config = {
     plugins: [
       replace({
@@ -67,7 +68,7 @@ packages.forEach((packageName) => {
       }),
     ],
     external: getExternal(packageName),
-  }
+  };
 
   if (build.input) {
     Object.entries(build.input).forEach(([kind, entry]) => {
@@ -75,13 +76,13 @@ packages.forEach((packageName) => {
         ...config,
         input: getPackageFile(packageName, entry),
         output: output(packageName, kind),
-      })
-    })
+      });
+    });
   } else {
     configs.push({
       ...config,
       input: input(packageName),
       output: output(packageName),
-    })
+    });
   }
-})
+});
