84ee39e39ca8b7ee5a18424e8a6aecf6551c35e5
Rahul Kadyan <hi@znck.me>
2020-05-22 14:56:32 +0530

    chore: overwrite typeorm dependency

--- pnpmfile.js
+++ pnpmfile.js
@@ -6,13 +6,7 @@ module.exports = {
 
 function readPackage(pkg) {
   if ('typeorm' in pkg.dependencies) {
-    return {
-      ...pkg,
-      dependencies: {
-        ...pkg.dependencies,
-        typeorm: 'znck/typeorm',
-      },
-    };
+    pkg.dependencies.typeorm = 'znck/typeorm'
   }
 
   return pkg;
